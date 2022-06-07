59b6525fccb62d9c71a9f911925a4f9d385fffe6
AlfredoGuacaran <79604260+AlfredoGuacaran@users.noreply.github.com>
2022-06-07 18:47:16 -0400

    instrucciones para la key mongodb

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import mongoose from 'mongoose';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import cosechasRoutes from './routes/cosechas.js';
 
+/// sustituir XXXXXXXMONGODB_KEYXXXXXX por la key de la base de datos
+const uri = 'mongodb+srv://alfredoguacaran:XXXXXXXMONGODB_KEYXXXXXX@cluster0.1ayw3.mongodb.net/?retryWrites=true&w=majority';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -14,8 +17,6 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.use('/', cosechasRoutes);
-
-const uri = 'mongodb+srv://alfredoguacaran:XXXXXXXMONGODB_KEYXXXXXX@cluster0.1ayw3.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
